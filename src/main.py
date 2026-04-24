7e64a6d816a4737506b043ad64f1be8a4e19648c
Elder Rayan Oliveira Silva <eld.ray.os@outlook.com>
2026-04-24 12:39:47 -0300

    fix: trying to overcome timeout issue

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import machine
 import time
+import sys
 
-print("--- Teeste de Pipeline CI/CD ---")
-
+print("--- Iniciando Simulação ---")
 led = machine.Pin(2, machine.Pin.OUT)
 
 for i in range(3):
     led.value(1)
-    print(f"Ciclo {i+1}: LED LIGADO")
-    time.sleep(1)
+    print("LED ON")
+    time.sleep(0.2)
     
     led.value(0)
-    print(f"Ciclo {i+1}: LED DESLIGADO")
-    time.sleep(1)
+    print("LED OFF")
+    time.sleep(0.2)
 
-print("--- Simulação concluída ---")
+print("--- Sucesso! Encerrando antes do Timeout ---")
+sys.exit(0) 
