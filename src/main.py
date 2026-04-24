afbd788f2d44f8009eb695afb7f0f61ac3c1ae8f
Elder Rayan Oliveira Silva <eld.ray.os@outlook.com>
2026-04-24 14:16:01 -0300

    fix: remove invalid hardware attributes and init telemetry pins

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ import machine
 import time
 import sys
 
+print("--- Inicializando Node Hardware ---")
+
 # Mapeamento de Hardware
 temp_sensor = machine.ADC(machine.Pin(34))
 load_sensor = machine.ADC(machine.Pin(35))
 led_cooler = machine.Pin(2, machine.Pin.OUT)
 led_alerta = machine.Pin(4, machine.Pin.OUT)
 
+time.sleep(1)
+
+print("Hardware OK")
+print('Type "help()" for more information.')
 sys.exit(0)
