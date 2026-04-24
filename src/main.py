b1a94ccebe1d1a443faa5a7a6a58dc9d9eaaa1bc
Elder Rayan Oliveira Silva <eld.ray.os@outlook.com>
2026-04-24 12:15:20 -0300

    initial test for the pipeline and Actions.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("Teste")
+import machine
+import time
+
+print("--- Teeste de Pipeline CI/CD ---")
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+for i in range(3):
+    led.value(1)
+    print(f"Ciclo {i+1}: LED LIGADO")
+    time.sleep(1)
+    
+    led.value(0)
+    print(f"Ciclo {i+1}: LED DESLIGADO")
+    time.sleep(1)
+
+print("--- Simulação concluída ---")
