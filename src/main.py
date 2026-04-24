d2fd59ccd8d31b8bdaf087e61f01b0fc17f3bb00
Elder Rayan Oliveira Silva <eld.ray.os@outlook.com>
2026-04-24 12:42:49 -0300

    fix: trying to overcome timeout issue again

--- src/main.py
+++ src/main.py
@@ -1,18 +1,1 @@
-import machine
-import time
-import sys
-
-print("--- Iniciando Simulação ---")
-led = machine.Pin(2, machine.Pin.OUT)
-
-for i in range(3):
-    led.value(1)
-    print("LED ON")
-    time.sleep(0.2)
-    
-    led.value(0)
-    print("LED OFF")
-    time.sleep(0.2)
-
-print("--- Sucesso! Encerrando antes do Timeout ---")
-sys.exit(0) 
+print("Teste de CI/CD")
