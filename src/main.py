90d59a02af04216051a72668758d1bfb9d7051d4
Elder Rayan Oliveira Silva <eld.ray.os@outlook.com>
2026-04-24 14:09:01 -0300

    feat: setup hardware components for edge telemetry simulation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+import machine
+import time
 import sys
-print("Teste")
+
+# Mapeamento de Hardware
+temp_sensor = machine.ADC(machine.Pin(34))
+load_sensor = machine.ADC(machine.Pin(35))
+led_cooler = machine.Pin(2, machine.Pin.OUT)
+led_alerta = machine.Pin(4, machine.Pin.OUT)
+
 sys.exit(0)
